428ba7fc5831e227f42896e5827a584a1d079761
Christoph Reiter <reiter.christoph@gmail.com>
2021-11-09 13:33:08 +0100

    Add a script for re-formatting rules
    This makes it easier to import things and then keep the diff small.

---
+++ fmt.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import glob
+import json
+
+
+DIR = os.path.dirname(os.path.realpath(__file__))
+
+def main(argv):
+    for name in ["TUGRAZ"]:
+        source = os.path.join(DIR, name)
+        for path in sorted(glob.glob(source + "/*.json")):
+            with open(path, "rb") as h:
+                sorted_rules = json.dumps(json.loads(h.read()), sort_keys=True, indent=4)
+            with open(path, "w", encoding="utf-8") as h:
+                h.write(sorted_rules)
+
+if __name__ == "__main__":
+    main(sys.argv)
